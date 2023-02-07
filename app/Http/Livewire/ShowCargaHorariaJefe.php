0db28b37fd9b8141bcb040c6336f2a58ff55a099
sakCode <angelitotupapitomana@gmail.com>
2023-02-07 15:41:59 -0500

    ResolucionBugCargaHorariaTablePage

--- app/Http/Livewire/ShowCargaHorariaJefe.php
+++ app/Http/Livewire/ShowCargaHorariaJefe.php
@@ -16,9 +16,15 @@ class ShowCargaHorariaJefe extends Component
 
     public function render()
     {
-        $cargas = CargaLectiva::orderBy('id','desc')
-        ->paginate($this->registros);
+        $cargas = CargaLectiva::orderBy('id', 'desc')
+            ->paginate($this->registros);
 
-        return view('livewire.show-carga-horaria-jefe',compact('cargas'));
+        return view('livewire.show-carga-horaria-jefe', compact('cargas'));
+    }
+
+    //Para evitar el bug de no existencia de cargas horarias
+    public function updatingRegistros()
+    {
+        $this->resetPage();
     }
 }
