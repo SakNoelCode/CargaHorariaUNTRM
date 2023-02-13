5b4b86635b7740abc89fd2d63b1b3a3dd17ecee8
sakCode <angelitotupapitomana@gmail.com>
2023-02-13 09:40:27 -0500

    HorasDatabaseandSeedDone

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,5 +30,6 @@ class DatabaseSeeder extends Seeder
         $this->call(EscuelaSeeder::class);
         $this->call(DocenteSeeder::class);
         $this->call(JefeDepartamentoSeeder::class);
+        $this->call(HoraSeeder::class);
     }
 }
---
+++ database/seeders/HoraSeeder.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Hora;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class HoraSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Hora::insert([
+            [
+                'hora_inicio' => '07:30',
+                'hora_fin' => '08:20'
+            ],
+            [
+                'hora_inicio' => '08:20',
+                'hora_fin' => '09:10'
+            ],
+            [
+                'hora_inicio' => '09:10',
+                'hora_fin' => '10:00'
+            ],
+            [
+                'hora_inicio' => '10:00',
+                'hora_fin' => '10:50'
+            ],
+            [
+                'hora_inicio' => '10:50',
+                'hora_fin' => '11:40'
+            ],
+            [
+                'hora_inicio' => '11:40',
+                'hora_fin' => '12:30'
+            ],
+            [
+                'hora_inicio' => '12:30',
+                'hora_fin' => '01:20'
+            ],
+            [
+                'hora_inicio' => '02:00',
+                'hora_fin' => '02:50'
+            ],
+            [
+                'hora_inicio' => '02:50',
+                'hora_fin' => '03:40'
+            ],
+            [
+                'hora_inicio' => '03:40',
+                'hora_fin' => '04:30'
+            ],
+            [
+                'hora_inicio' => '04:30',
+                'hora_fin' => '05:20'
+            ],
+            [
+                'hora_inicio' => '05:20',
+                'hora_fin' => '06:10'
+            ],
+            [
+                'hora_inicio' => '06:10',
+                'hora_fin' => '07:00'
+            ],
+            [
+                'hora_inicio' => '07:00',
+                'hora_fin' => '07:50'
+            ],
+            [
+                'hora_inicio' => '07:50',
+                'hora_fin' => '08:40'
+            ],
+            [
+                'hora_inicio' => '08:40',
+                'hora_fin' => '09:30'
+            ]
+        ]);
+    }
+}
