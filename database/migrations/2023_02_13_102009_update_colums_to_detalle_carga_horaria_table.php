d1e211a2d5380bfc224c3dd9544fdff98e94cd91
sakCode <angelitotupapitomana@gmail.com>
2023-02-13 10:24:10 -0500

    UpdateColumDetalleCargaHoraria

---
+++ database/migrations/2023_02_13_102009_update_colums_to_detalle_carga_horaria_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('detalle_carga_horaria', function (Blueprint $table) {
+            $table->dropColumn('hora_inicio');
+            $table->dropColumn('hora_fin');
+        });
+
+        Schema::table('detalle_carga_horaria', function (Blueprint $table) {
+            $table->foreignId('hora_inicio_id')->constrained('horas')->onDelete('cascade');
+            $table->foreignId('hora_fin_id')->constrained('horas')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('detalle_carga_horaria', function (Blueprint $table) {
+            $table->time('hora_inicio')->after('tipo');
+        });
+
+        Schema::table('detalle_carga_horaria', function (Blueprint $table) {
+            $table->time('hora_fin')->after('hora_inicio');
+        });
+
+        Schema::table('detalle_carga_horaria', function (Blueprint $table) {
+            $table->dropColumn('hora_inicio_id');
+            $table->dropColumn('hora_fin_id');
+        });
+    }
+};
